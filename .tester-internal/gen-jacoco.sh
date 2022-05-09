e1ad18a0f80036fd777fca91c36b124f93bb47ed
alan <alan_chen1@brown.edu>
2022-05-09 14:58:19 -0400

    add jacoco ci integration

--- .tester-internal/gen-jacoco.sh
+++ .tester-internal/gen-jacoco.sh
@@ -2,5 +2,7 @@
 cd .tester-internal
 java -jar jacococli.jar merge $(find jacoco/*.exec) --destfile jacoco/merged.exec
 rm -rf ../integration-jacoco
+# export reports, can change paths later
 java -jar jacococli.jar report jacoco/merged.exec --classfiles ../target/classes --html ../integration-jacoco/
+java -jar jacococli.jar report jacoco/merged.exec --classfiles ../target/classes --xml ../.tester-internal/jacoco-report.xml
 cd ..
